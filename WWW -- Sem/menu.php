f0f20c2c569323b0f8be6d3a8901919a99e886a0
DOL7JS <53859920+DOL7JS@users.noreply.github.com>
2021-02-01 12:42:39 +0100

    Updt menu,index

--- WWW -- Sem/menu.php
+++ WWW -- Sem/menu.php
@@ -1,9 +1,8 @@
 
-<label for="show-menu" class="show-menu">Menu</label>
-<input type="checkbox" id="show-menu" role="button">
+<label for="show-menu" class="show-menu">Menu</label><!--//kod pro responsivni menu převzat: https://medialoot.com/blog/how-to-create-a-responsive-navigation-menu-using-only-css/-->
+<input type="checkbox" id="show-menu">
 
 <ul id="menu">
-    <!--<li><a href="index.php?page=homePage"><div id="logo"><img  height="50px" width="50px" src="imgs/logo.png"></div></a></li>-->
     <li><a href="index.php?pages=homePage">Domů</a></li>
     <li><a href="index.php?pages=categories&gender=m">Muži</a></li>
     <li><a href="index.php?pages=categories&gender=f">Ženy</a></li>
@@ -14,36 +13,36 @@
 
         if($_SESSION["role"]=="Admin"){
             echo '
-                          <li style="float:right" ><a href="index.php?pages=logOut">Odhlásit se</a></li>  
-                          <li style="float:right"> <a href="index.php?pages=account">Můj účet</a></li><li style="float:right"> <a href="index.php?pages=usersManagement">Správa uživatelů</a></li>
-                          <li style="float:right" ><a href="index.php?pages=goodsManagement">Správa zboží</a></li>
-                          <li style="float:right" ><a href="index.php?pages=saleManagement">Správa akcí</a></li>
-                          <li style="float:right" ><a href="index.php?pages=ordersManagement">Správa objednávek</a></li>';
+                          <li class="fRight" ><a href="index.php?pages=logOut">Odhlásit se</a></li>  
+                          <li class="fRight"> <a href="index.php?pages=account">Můj účet</a></li><li class="fRight"> <a href="index.php?pages=usersManagement">Správa uživatelů</a></li>
+                          <li class="fRight" ><a href="index.php?pages=goodsManagement">Správa zboží</a></li>
+                          <li class="fRight" ><a href="index.php?pages=saleManagement">Správa akcí</a></li>
+                          <li class="fRight" ><a href="index.php?pages=ordersManagement">Správa objednávek</a></li>';
         }else if($_SESSION["role"]=="Zaměstnanec"){
-            echo '<li style="float:right" ><a href="index.php?pages=logOut">Odhlásit se</a></li>  
-                            <li style="float:right"> <a href="index.php?pages=account">Můj účet</a></li>
-                            <li style="float:right" ><a href="index.php?pages=goodsManagement">Správa zboží</a></li>
-                            <li style="float:right" ><a href="index.php?pages=saleManagement">Správa akcí</a></li>
-                            <li style="float:right" ><a href="index.php?pages=ordersManagement">Správa objednávek</a></li>';
+            echo '<li class="fRight" ><a href="index.php?pages=logOut">Odhlásit se</a></li>  
+                            <li class="fRight"> <a href="index.php?pages=account">Můj účet</a></li>
+                            <li class="fRight" ><a href="index.php?pages=goodsManagement">Správa zboží</a></li>
+                            <li class="fRight" ><a href="index.php?pages=saleManagement">Správa akcí</a></li>
+                            <li class="fRight" ><a href="index.php?pages=ordersManagement">Správa objednávek</a></li>';
         }else{
             if(!empty($_SESSION["cart"])){
-                echo '    <li style="float:right;border: black solid 1px"><a class="active" href="index.php?pages=cart">Košík -- '.count($_SESSION["cart"]).'</a></li>';
+                echo '    <li class="fRight blackSolid"><a class="active" href="index.php?pages=cart">Košík -- '.count($_SESSION["cart"]).'</a></li>';
             }else{
-                echo '    <li style="float:right;border: black solid 1px"><a class="active" href="index.php?pages=cart">Košík</a></li>';
+                echo '    <li class="fRight blackSolid"><a class="active" href="index.php?pages=cart">Košík</a></li>';
             }
 
-            echo '<li style="float:right" ><a href="index.php?pages=logOut">Odhlásit se</a></li>  
-                          <li style="float:right"> <a href="index.php?pages=account">Můj účet</a></li>
-                          <li style="float:right" ><a href="index.php?pages=myOrders">Moje objednávky</a></li>';
+            echo '<li class="fRight"><a href="index.php?pages=logOut">Odhlásit se</a></li>  
+                          <li class="fRight"> <a href="index.php?pages=account">Můj účet</a></li>
+                          <li class="fRight" ><a href="index.php?pages=myOrders">Moje objednávky</a></li>';
         }
     }else{
         if(!empty($_SESSION["cart"])){
-            echo '    <li style="float:right;border: black solid 1px"><a class="active" href="index.php?pages=cart">Košík -- '.count($_SESSION["cart"]).'</a></li>';
+            echo '    <li class="fRight blackSolid"><a class="active" href="index.php?pages=cart">Košík -- '.count($_SESSION["cart"]).'</a></li>';
         }else{
-            echo '    <li style="float:right;border: black solid 1px"><a class="active" href="index.php?pages=cart">Košík</a></li>';
+            echo '    <li class="fRight blackSolid"><a class="active" href="index.php?pages=cart">Košík</a></li>';
         }
-        echo '<li style="float:right"> <a href="index.php?pages=logIn">Přihlášení</a></li>
-                  <li style="float:right" ><a href="index.php?pages=signUp">Registrace</a></li>';
+        echo '<li class="fRight"> <a href="index.php?pages=logIn">Přihlášení</a></li>
+                  <li class="fRight" ><a href="index.php?pages=signUp">Registrace</a></li>';
     }
     ?>
 </ul>
